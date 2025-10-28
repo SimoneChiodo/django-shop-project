d5010895901e0fa545f7327797a02e4f07b0987b
SimoneChiodo <simochi.005@gmail.com>
2025-10-28 16:15:41 +0100

    Add customer model

--- shop_project/orders/models.py
+++ shop_project/orders/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+  name = models.CharField(max_length=100)
+  email = models.EmailField(unique=True)
+  total_spent = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+  def __str__(self):
+    return self.name
+  
