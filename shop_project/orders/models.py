156bcafbdfb9e11ee1793e4cfd58194ec9ddd963
SimoneChiodo <simochi.005@gmail.com>
2025-10-28 16:22:56 +0100

    Add order model

--- shop_project/orders/models.py
+++ shop_project/orders/models.py
@@ -15,3 +15,12 @@ class Product(models.Model):
   def __str__(self):
     return self.name
   
+class Order(models.Model):
+  customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+  product = models.ForeignKey(Product, on_delete=models.CASCADE)
+  quantity = models.PositiveIntegerField(default=1)
+  total_price = models.DecimalField(max_digits=10, decimal_places=2)
+  created_at = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return f"Ordine #{self.id} - {self.customer.name}"
